d30928a1d3ab0ba653c207d8462c0882f263b4c7
BaptisteBriois <baptiste.briois@hotmail.fr>
2015-09-15 15:12:20 +0200

    bdd Mineproject

---
+++ mineproject.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 15, 2015 at 01:02 PM
+-- Server version: 5.6.17-debug-log
+-- PHP Version: 5.6.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `mineproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `minetable`
+--
+
+CREATE TABLE IF NOT EXISTS `minetable` (
+`ID` int(10) NOT NULL,
+  `nameObject` varchar(255) NOT NULL,
+  `namePerson` varchar(255) NOT NULL,
+  `photoObject` varchar(255) NOT NULL,
+  `photoPerson` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `minetable`
+--
+ALTER TABLE `minetable`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `minetable`
+--
+ALTER TABLE `minetable`
+MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
