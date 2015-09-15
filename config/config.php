8a41a779ce20e3551b57062dec9f6f2d70940e25
ArnaudDoubremelle <a-r-n-a-u-d-@hotmail.fr>
2015-09-15 15:34:50 +0200

    Modification config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,23 @@
 <?php
 
 
-	$dbname = "mineproject";
-	$host	= "localhost";
-	$user	= "root";
-	$pass	= "root";
-    $port   = "3306";
-	
-	$db = new PDO('mysql:host='.$host.';port='.$port.';dbname='.$dbname, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));	
-	
+
+// Paramétrage des différentes valeurs de connexion à la base de donénes
+define('SQL_HOST',       'localhost');
+define('SQL_USERNAME',   'root');
+define('SQL_PASSWORD',   'root');
+define('SQL_DBNAME',	 'mineproject'); 
+
+
+
+// Connexion à la base de données
+try {
+	$db = new PDO('mysql:dbname='.SQL_DBNAME.';host='.SQL_HOST, SQL_USERNAME, SQL_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+}
+catch(Exception $e) {
+	exit('Erreur : ' . $e->getMessage());
+}
+
+
+
 ?>
