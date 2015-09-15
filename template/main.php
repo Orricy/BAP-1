f53602ce385aafb4cf7a5461c34e49fa7cec9859
Damien Duvernois <d.duvernois@student.isartdigital.com>
2015-09-15 17:29:48 +0200

    Edit css

--- template/main.php
+++ template/main.php
@@ -1,13 +1,27 @@
 <body>
-	<video id="video" width="320" height="240" autoplay></video>
-	<button id="what">What</button>
-	<canvas id="canvas" width="320" height="240" style="border: 5px solid black;"></canvas>
+<div class="container">
+  	<div class="col-xs-12">
+  		<h2 class="text-center">Mine</h2>
+  	</div>
+  	<div class="col-xs-12">
+	  	<button type="button" class="btn btn-lg btn-block">What?<br><span class="glyphicon glyphicon-camera" aria-hidden="true"></span></button>
+	  	<video id="video" width="340" height="240" autoplay class="col-xs-offset-4"></video>
+	  	<br>
+		<button id="what" class="col-xs-offset-5 col-xs-2">Shoot</button>
+		<canvas id="canvas" width="340" height="240" style="border: 5px solid black;"></canvas>
+  	</div>
+  	<div class="col-xs-12">
+	  	<button type="button" class="btn btn-lg btn-block">Who?<br><span class="glyphicon glyphicon-camera" aria-hidden="true"></span></button>
+	  	<video id="video2" width="340" height="240" autoplay class="col-xs-offset-4"></video>
+	  	<br>
+		<button id="who" class="col-xs-offset-5 col-xs-2">Shoot</button>
+		<canvas id="canvas2" width="340" height="240" style="border: 5px solid black;"></canvas>
+  	</div>
+</div>
 	<form method="POST" action="try.php">
 		<input type="hidden" name="what" value="" id="what-form">
 		<input type="hidden" name="who" value="" id="who-form">
 		<input type="submit" name="deconnexion" value="Me déconnecter">
 	</form>
-	<video id="video2" width="320" height="240" autoplay></video>
-	<button id="who">Who</button>
-	<canvas id="canvas2" width="320" height="240" style="border: 5px solid black;"></canvas>
+	
 </body>
