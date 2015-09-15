db1d6dafd55f22783234a96896eaf35979589189
Damien Duvernois <d.duvernois@student.isartdigital.com>
2015-09-15 15:13:06 +0200

    Ajout d'un second canvas

--- template/main.php
+++ template/main.php
@@ -1,10 +1,10 @@
 <body>
 	<video id="video" width="640" height="480" autoplay></video>
 	<button id="snap">Snap Photo</button>
-	<canvas id="canvas" width="640" height="480"></canvas>
+	<canvas id="canvas" width="640" height="480" style="border: 5px solid black;"></canvas>
 	<form method="POST" action="try.php">
 		<input type="hidden" name="object" value="" id="object">
 		<input type="submit" name="deconnexion" value="Me déconnecter">
 	</form>
-	
+	<canvas id="canvas2" width="640" height="480" style="border: 5px solid black;"></canvas>
 </body>
